4862d5def62efe278d323b27490e6f827225b66e
Ali <saida130.210@gmail.com>
2020-01-08 16:28:22 +0000

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
+const modal = document.querySelector('#modal')
+modal.classList.add('hidden')
 
 
 //------------------------------------------------------------------------------
