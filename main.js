78228ed73fdd0c6f9be08a3e9add4f43409f3cbe
github username <github email address>
2020-01-09 17:00:46 +0000

    Done.

--- main.js
+++ main.js
@@ -1,8 +1,48 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  
+  const modal = document.querySelector('#modal')
+  modal.hidden = true;
+
+  let likeButtons = document.querySelectorAll('.like-glyph')
+    
+  for(const element of likeButtons) {
+    element.addEventListener("click", function(e){
+    mimicServerCall()
+    .then(function(response){
+      if (e.target.classList.contains("activated-heart")) {
+         e.target.classList.remove("activated-heart")
+      } else {
+        e.target.classList.add("activated-heart")
+      }
+    })                          
+    .catch(function(error){
+      modal.hidden = false;
+      modal.innerText = error;
+      setTimeout(function(){ modal.hidden = true}, 5000);
+
+    });  
+    })
+
+  }
+  
+
+  
+                      
+
+
+});
+
+
+
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+
+
+
+
 
 
 
